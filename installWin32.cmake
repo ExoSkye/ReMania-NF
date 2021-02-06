155b8680b05b219c9039026c92d4ea52224a81cc
protobit <admin@protobyte.tech>
2021-02-06 09:10:58 +0000

    Fixed Windows again ffs

--- installWin32.cmake
+++ installWin32.cmake
@@ -5,15 +5,15 @@ add_custom_command(TARGET ReMania POST_BUILD
 if(CMAKE_BUILD_TYPE MATCHES "Debug")
     add_custom_command(TARGET ReMania POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy_if_different
-            "${CMAKE_BINARY_DIR}/lib/glew/lib/libglewd.dll"
+            "${CMAKE_BINARY_DIR}/lib/glew/bin/${CMAKE_BUILD_TYPE}/glewd.dll"
             ${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}/out/)
 else()
     add_custom_command(TARGET ReMania POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy_if_different
-            "${CMAKE_BINARY_DIR}/lib/glew/lib/libglew.dll"
+            "${CMAKE_BINARY_DIR}/lib/glew/bin/${CMAKE_BUILD_TYPE}/glew.dll"
             ${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}/out/)
 endif()
 add_custom_command(TARGET ReMania POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
-        "${CMAKE_BINARY_DIR}/lib/glfw3/src/libglfw.dll"
+        "${CMAKE_BINARY_DIR}/lib/glfw3/src/${CMAKE_BUILD_TYPE}/glfw3.dll"
         ${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}/out/)
