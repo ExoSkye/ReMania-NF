a93a4b4328401239937473b1f12cd19af77be4be
protobit <admin@protobyte.tech>
2021-02-06 09:06:38 +0000

    Fixed Windows artifacts

--- installWin32.cmake
+++ installWin32.cmake
@@ -1,19 +1,19 @@
 add_custom_command(TARGET ReMania POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
-        "${CMAKE_BINARY_DIR}/ReMania-NF.exe"
+        "${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}/ReMania-NF.exe"
         ${CMAKE_BINARY_DIR}/out/)
 if(CMAKE_BUILD_TYPE MATCHES "Debug")
     add_custom_command(TARGET ReMania POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy_if_different
             "${CMAKE_BINARY_DIR}/lib/glew/lib/libglewd.dll"
-            ${CMAKE_BINARY_DIR}/out/)
+            ${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}/out/)
 else()
     add_custom_command(TARGET ReMania POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy_if_different
             "${CMAKE_BINARY_DIR}/lib/glew/lib/libglew.dll"
-            ${CMAKE_BINARY_DIR}/out/)
+            ${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}/out/)
 endif()
 add_custom_command(TARGET ReMania POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
         "${CMAKE_BINARY_DIR}/lib/glfw3/src/libglfw.dll"
-        ${CMAKE_BINARY_DIR}/out/)
+        ${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}/out/)
