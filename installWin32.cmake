743698ddd3895b286700621405fa3b5ef8f098b9
protobit <admin@protobyte.tech>
2021-02-06 09:18:53 +0000

    Hopefully fixed Windows CI ffs

--- installWin32.cmake
+++ installWin32.cmake
@@ -6,14 +6,14 @@ if(CMAKE_BUILD_TYPE MATCHES "Debug")
     add_custom_command(TARGET ReMania POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy_if_different
             "${CMAKE_BINARY_DIR}/lib/glew/bin/${CMAKE_BUILD_TYPE}/glewd.dll"
-            ${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}/out/)
+            ${CMAKE_BINARY_DIR}/out/)
 else()
     add_custom_command(TARGET ReMania POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy_if_different
             "${CMAKE_BINARY_DIR}/lib/glew/bin/${CMAKE_BUILD_TYPE}/glew.dll"
-            ${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}/out/)
+            ${CMAKE_BINARY_DIR}/out/)
 endif()
 add_custom_command(TARGET ReMania POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
         "${CMAKE_BINARY_DIR}/lib/glfw3/src/${CMAKE_BUILD_TYPE}/glfw3.dll"
-        ${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}/out/)
+        ${CMAKE_BINARY_DIR}/out/)
