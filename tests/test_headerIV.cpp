7b5ec6554d87565d3860b3fdb33d9ea668d93555
protobit <admin@protobyte.tech>
2021-03-01 20:26:45 +0000

    Changed to CXX to possibly help windows builds, and fixed headerIV tests

--- tests/test_headerIV.cpp
+++ tests/test_headerIV.cpp
@@ -4,7 +4,7 @@ int main(int argc, char** argv) {
     std::ifstream pack(std::string(argv[1])+"/assets/Packs/Game.pak");
     std::string ivStr = assetLayer::getHeaderIV(pack);
     uint64_t iv = (uint64_t)&ivStr[0];
-    if (iv != 140737488348144ull) {
+    if (iv != 140737488348032ull) {
         printf("Test failed");
     }
     else {
